3c7f504f0e681129e6e269e69a0266eb0013770f
Josh Eskew <jeskew82@gmail.com>
2021-02-25 14:45:24 -0500

    Started script file. All elemnets in use are selected and event listeners have been added

---
+++ script.js
@@ -0,0 +1,17 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp');
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
