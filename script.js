c223339b626f28faffa6bc0fc2d9100b1b033f83
Josh Eskew <jeskew82@gmail.com>
2021-02-25 20:15:52 -0500

    Added first 3 functions to script. Play and Stop button working.

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
 
 progress.addEventListener('change', setVideoProgress);
+
+// This plays and pauses the video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    };
+}
+
+// This toggles play/pause button
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+// Stops video and returns it to the beginning
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+function updateProgress() {
+    return true;
+}
+
+function setVideoProgress() {
+    return true;
+}
+
